0761ec9a4de2a618569a13ea9e516e4e4658958d
“miguelalonso” <miguel_al_al@hotmail.com>
2022-03-14 13:14:51 +0100

    restaurar

--- adivinacolor.js
+++ adivinacolor.js
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-"use strict";
+("use strict");
 let contadorAciertos;
 let contadorErrores;
 let colorCorrecto;
@@ -110,68 +109,3 @@ function nuevaPartida() {
 }
 
 nuevaPartida();
-=======
-// "use strict";
-// let contadorAciertos = 0;
-// let contadorErrores = 0;
-// let colorCorrecto;
-// let colores;
-// //Función que genera un color
-// function generaColores() {
-//   let colores = [];
-//   for (let i = 0; i < 3; i++) {
-//     colores[i] = [
-//       (Math.random() * 256).toFixed(0),
-//       (Math.random() * 256).toFixed(0),
-//       (Math.random() * 256).toFixed(0),
-//     ];
-//   }
-//   return colores;
-// }
-// //Función compueba los colores
-// function comprobador(colorSeleccionado) {
-//   if (colorSeleccionado === colorCorrecto) {
-//     contadorAciertos++;
-//     console.log(
-//       "acertaste " +
-//         " aciertos " +
-//         contadorAciertos +
-//         " fallos " +
-//         contadorErrores
-//     );
-//     if (contadorAciertos === 3) {
-//       alert("you win");
-//     }
-//   } else {
-//     contadorErrores++;
-//     console.log(
-//       "fallaste " +
-//         " aciertos " +
-//         contadorAciertos +
-//         " fallos " +
-//         contadorErrores
-//     );
-//     if (contadorErrores === 3) {
-//       alert("GAME OVER");
-//     }
-//   }
-// }
-// function simulador() {
-//   while (contadorAciertos < 3 && contadorErrores < 3) {
-//     colores = generaColores();
-//     colorCorrecto = (Math.random() * 2).toFixed(0);
-//     console.log("RGB:" + colores[colorCorrecto]);
-//     console.log(
-//       "opciones a elegir \n " +
-//         colores[0] +
-//         "\n" +
-//         colores[1] +
-//         "\n" +
-//         colores[2]
-//     );
-//     //colorSeleccionado = click recibido(0,1 o 2)
-//     comprobador(prompt("elege color")); //es solo comprobador
-//   }
-// }
-// simulador();
->>>>>>> ed36ab0ef11baa0de7c75d5b7ebf3f101a258040
