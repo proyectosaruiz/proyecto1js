f6760a7af19ef0ed810321d649aad1f738d3e8d0
“miguelalonso” <miguel_al_al@hotmail.com>
2022-03-21 12:55:53 +0100

    comentarios js, cambios css y cambios html

--- adivinacolor.js
+++ adivinacolor.js
@@ -1,49 +1,45 @@
 "use strict";
 
-let contadorAciertos;
-let contadorErrores;
-let colorCorrecto;
-//let colores;
+let contadorAciertos; //variable aciertos
+let contadorErrores; //variable contador
+let colorCorrecto; //variable RGB del color buscado
+
+//Escuchadores de click en colores, se llama a comprobador como parámetro la posición donde se hizo click
 
 const muestra1 = document.querySelector(".muestra1");
-
 muestra1.addEventListener("click", () => {
   if (contadorAciertos < 3 && contadorErrores < 3) {
     comprobador(0);
   }
 });
-const muestra2 = document.querySelector(".muestra2");
 
+const muestra2 = document.querySelector(".muestra2");
 muestra2.addEventListener("click", () => {
   if (contadorAciertos < 3 && contadorErrores < 3) {
     comprobador(1);
   }
 });
-const muestra3 = document.querySelector(".muestra3");
 
+const muestra3 = document.querySelector(".muestra3");
 muestra3.addEventListener("click", () => {
   if (contadorAciertos < 3 && contadorErrores < 3) {
     comprobador(2);
   }
 });
 
-const iniciaPartida = document.querySelector(".iniciarPartida");
+//Escuchador del botón de nueva partida
 
+const iniciaPartida = document.querySelector(".iniciarPartida");
 iniciaPartida.addEventListener("click", () => {
   nuevaPartida();
 });
 
-//let colorSeleccionado;
-//Función que genera un color
+//Función que genera un color aleatorio y otros dos similares en un rango de 50 y +-20 del color aleatorio
+//limitamos el num por encima de 256 y por debajo de 0
+
 function generaColores() {
   let colores = [];
-  // for (let i = 0; i < 3; i++) {
-  //   colores[i] = [
-  //     (Math.random() * 256).toFixed(0),
-  //     (Math.random() * 256).toFixed(0),
-  //     (Math.random() * 256).toFixed(0),
-  //   ];
-  // }
+
   colores[0] = [
     Math.floor(Math.random() * 256),
     Math.floor(Math.random() * 256),
@@ -55,21 +51,38 @@ function generaColores() {
     colores[0][1] + 20 + Math.floor(Math.random() * 50),
     colores[0][2] + 20 + Math.floor(Math.random() * 50),
   ];
+  if (colores[1][0] > 255) {
+    colores[1][0] = 255;
+  }
+  if (colores[1][1] > 255) {
+    colores[1][1] = 255;
+  }
+  if (colores[1][2] > 255) {
+    colores[1][2] = 255;
+  }
 
   colores[2] = [
     colores[0][0] - 20 + Math.floor(Math.random() * -50),
     colores[0][1] - 20 + Math.floor(Math.random() * -50),
     colores[0][2] - 20 + Math.floor(Math.random() * -50),
   ];
-  console.log(colores[0]);
-  console.log(colores[1]);
-
-  console.log(colores[2]);
+  if (colores[2][0] < 0) {
+    colores[2][0] = 0;
+  }
+  if (colores[2][1] < 0) {
+    colores[2][1] = 0;
+  }
+  if (colores[2][2] < 0) {
+    colores[2][2] = 0;
+  }
 
   return colores;
 }
 
-//Función compueba los colores
+//Función que compueba si el color clickado por el usuario es el mismo que el color correcto
+// y gestiona los contadores y el marcador de lose o win
+//Recibe como parámetro la posición en la que hizo click el usuario
+
 function comprobador(colorSeleccionado) {
   if (colorSeleccionado === colorCorrecto) {
     contadorAciertos++;
@@ -90,32 +103,12 @@ function comprobador(colorSeleccionado) {
       resultado.textContent = "YOU LOSE";
     }
   }
-  // console.log(contadorAciertos);
-  // console.log(contadorErrores);
+
   nuevoIntento();
 }
 
-// function simulador() {
-//   while (contadorAciertos < 3 && contadorErrores < 3) {
-//     colores = generaColores();
-//     colorCorrecto = (Math.random() * 2).toFixed(0);
-
-//     console.log("RGB:" + colores[colorCorrecto]);
-//     console.log(
-//       "opciones a elegir \n " +
-//         colores[0] +
-//         "\n" +
-//         colores[1] +
-//         "\n" +
-//         colores[2]
-//     );
-
-//     //colorSeleccionado = click recibido(0,1 o 2)
-
-//     comprobador(prompt("elege color")); //es solo comprobador
-//   }
-// }
-//simulador();
+// dibuja los colores y el codigo RGB, recibe como parámetro un array de arrays con los 3 colores y
+// la posición del color correcto
 
 function pintaColores(colores, colorCorrecto) {
   muestra1.style.backgroundColor = `rgb(${colores[0][0]},${colores[0][1]},${colores[0][2]})`;
@@ -124,19 +117,19 @@ function pintaColores(colores, colorCorrecto) {
 
   muestra3.style.backgroundColor = `rgb(${colores[2][0]},${colores[2][1]},${colores[2][2]})`;
 
-  //aqui es donde en lugar del color backgroundcolor, tiene que apararecer el texto rgb
-  //const enunciado = document.querySelector(".enunciado");
   const codigoRgb = document.querySelector("#rgb");
   codigoRgb.textContent = `RGB: ${colores[colorCorrecto][0]} . ${colores[colorCorrecto][1]} . ${colores[colorCorrecto][2]}`;
 }
 
+// selecciona si la posicion RGB correctra es la 1, 2 o 3 y tambien posiciona el resto de colores
+
 function nuevoIntento() {
-  colorCorrecto = Math.floor(Math.random() * 2);
+  colorCorrecto = Math.floor(Math.random() * 3);
 
   pintaColores(generaColores(), colorCorrecto);
 }
 
-//colorCorrecto = (Math.random() * 2).toFixed(0);
+// funcion que pone los contadores a 0, gestiona el marcador, oculta el final de partida y inicia nuevos colores
 
 function nuevaPartida() {
   contadorErrores = 0;
@@ -151,4 +144,4 @@ function nuevaPartida() {
   nuevoIntento();
 }
 
-nuevaPartida();
+nuevaPartida(); //llamada a partida nueva
